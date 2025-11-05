c362b4f92ea9f3dce87a166ae3ead5af7c440e85
dennis-del <dennisjames991@gmail.com>
2025-11-05 11:06:37 +0530

    extra ssl

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -12,9 +12,14 @@ export const AppDataSource = new DataSource(
     ? {
         type: "postgres",
         url: process.env.DATABASE_URL,
-        synchronize: false, // recommended for production
+        synchronize: false,
         logging: false,
-        ssl: { rejectUnauthorized: false },
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
         entities: [path.join(__dirname, "../models/*.{js,ts}")],
         migrations: [path.join(__dirname, "../migrations/**/*.{js,ts}")],
       }
@@ -25,12 +30,13 @@ export const AppDataSource = new DataSource(
         username: process.env.DB_USERNAME || process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        synchronize: true, // ok for local dev
+        synchronize: true,
         logging: true,
         entities: [path.join(__dirname, "../models/*.{ts,js}")],
         migrations: [path.join(__dirname, "../migrations/**/*.{ts,js}")],
       }
 );
+
 
 
 
